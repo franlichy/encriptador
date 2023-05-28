9dd8756912c399f6095e4ca8a6753d060ae69053
franlichy <franco.liciaga@gmail.com>
2023-05-28 17:58:15 -0300

    subiendo encriptador

---
+++ main.js
@@ -0,0 +1,62 @@
+import { encrypt, decrypt } from "./utils/encriptar.js"
+
+// Localizo elementos principales
+const input = document.getElementById("inputMain")
+const result = document.getElementById("result")
+
+const botonEncriptar = document.getElementById("encryptBtn")
+const decryptBtn = document.getElementById("decryptBtn")
+
+const copyBtn = document.getElementById("copyBtn")
+
+// Configuro variables y estados
+const inputSaved = localStorage.getItem("inputSaved")
+const resultSaved = localStorage.getItem("resultSaved")
+
+const resultContentDefault = `
+<img src="img/defaultResultImg.svg" alt="default result" class="resultImgDefault" />
+<h2>Ningún mensaje fue encontrado</h2>
+<p>Ingresa el texto que desees encriptar o desencriptar.</p>
+`
+if (inputSaved) input.value = inputSaved
+if (!resultSaved) copyBtn.style.display = "none"
+
+const setResult = text => {
+  result.innerHTML = text
+    ? `<h2 class="resultText">${text}</h2>`
+    : resultContentDefault
+  text
+    ? localStorage.setItem("resultSaved", text)
+    : localStorage.removeItem("resultSaved")
+}
+const updateInput = () => {
+  input.value
+    ? localStorage.setItem("inputSaved", input.value)
+    : localStorage.removeItem("inputSaved")
+}
+const updateCopyBtn = () => {
+  copyBtn.style.display = input.value ? "block" : "none"
+}
+
+setResult(resultSaved)
+
+// Creación de funciones de los botones
+const encryptSubmit = () => {
+  setResult(encrypt(input.value))
+  updateCopyBtn()
+}
+
+const decryptSubmit = () => {
+  setResult(decrypt(input.value))
+  updateCopyBtn()
+}
+const copyText = () => {
+  const result = localStorage.getItem("resultSaved")
+  navigator.clipboard.writeText(result)
+}
+
+// Configurando botones con sus funciones
+botonEncriptar.onclick = encryptSubmit
+decryptBtn.onclick = decryptSubmit
+copyBtn.onclick = copyText
+input.oninput = updateInput
